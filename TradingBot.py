8669b15ea2397d670d3fd65933de1002aeb663a3
jazlynj.m.lin <jazlyn.jm.lin@gmail.com>
2019-02-18 18:58:40 +1100

    Added visualisation to triangular arbitrage opportunities

--- TradingBot.py
+++ TradingBot.py
@@ -1,9 +1,17 @@
 import time
 from datetime import datetime
-
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab as pl
+from IPython import display
 from collections import deque
 from binance.client import Client
 from BinanceKeys import BinanceKeySecretPair
+
+plt.ion()
+fig=plt.figure()
+
+
 api_key = BinanceKeySecretPair['api_key']
 api_secret = BinanceKeySecretPair['api_secret']
 client = Client(api_key, api_secret)
@@ -51,22 +59,40 @@ def get_potential_arbitrages(symbols, tickers):
 
 
 def find_best_arbitrages(triangles, index_map, tickers):
+    transaction_fee = 0.1
+    max_return_list = np.array([])
+    time_list = np.array([])
 
-    max_return_rate = -100
-
+    iteration = 0
     while True:
         tickers = client.get_orderbook_tickers()
+
+        max_return_rate =  -1000
+
         for triangle in triangles:
             rate1 = float(tickers[index_map[triangle[0]]]['askPrice'])
             rate2 = float(tickers[index_map[triangle[1]]]['bidPrice'])*float(tickers[index_map[triangle[2]]]['bidPrice'])
             if round(rate1, 7) == 0:
                 continue
 
-            return_rate = (rate2-rate1)/(rate1)*100.0
+            return_rate = (rate2-rate1)/(rate1)*100.0 - transaction_fee*2
 
             if return_rate > max_return_rate:
                 max_return_rate = return_rate
                 print("Maximum: " + str(max_return_rate) + "% arbitrage from " + triangle[1] + "->" + triangle[2])
+
+        max_return_list = np.append(max_return_list, max_return_rate)
+        time_list = np.append(time_list, iteration)
+        iteration += 1
+        plt.plot(time_list, max_return_list,'b-*')
+        plt.xlabel("Time (Seconds)", fontweight="bold")
+        plt.ylabel("Net Return (%)", fontweight="bold")
+        plt.title("Triangular Arbitrage Live Return Rate", fontweight="bold")
+        plt.show()
+        plt.grid()
+        plt.pause(0.0001)
+        time.sleep(1)
+        plt.grid()
 
 
 conversions = client.get_orderbook_tickers()
@@ -74,4 +100,3 @@ conversion_to_index = get_symbol_to_index(conversions)
 symbol_list = conversion_to_index.keys()
 arb_triangles = get_potential_arbitrages(symbol_list, conversions)
 find_best_arbitrages(arb_triangles, conversion_to_index, conversions)
-
